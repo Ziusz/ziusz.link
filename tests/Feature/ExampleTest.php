ccd789dac48c052351f475c031efd9f1fb4b42a6
Ziusz <ziusz@outlook.com>
2026-06-09 17:59:21 +0200

    Update project metadata

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,5 +3,7 @@
 test('returns a successful response', function () {
     $response = $this->get(route('home'));
 
-    $response->assertOk();
+    $response
+        ->assertOk()
+        ->assertSeeText('Private links');
 });
